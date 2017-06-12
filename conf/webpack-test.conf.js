14767974feefa382dc681f44fa1a9b776b69e625
cchardeau <corentin.chardeau@epitech.eu>
2017-06-12 04:48:48 +0200

    Add a new version of header

--- conf/webpack-test.conf.js
+++ conf/webpack-test.conf.js
@@ -27,6 +27,12 @@ module.exports = {
         loaders: [
           'html-loader'
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: [
+          'file-loader?name=assets/[name].[hash].[ext]'
+        ]
       }
     ]
   },
--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -42,6 +42,12 @@ module.exports = {
         loaders: [
           'html-loader'
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: [
+          'file-loader?name=assets/[name].[hash].[ext]'
+        ]
       }
     ]
   },
